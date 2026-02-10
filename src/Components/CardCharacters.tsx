753a18350df18bef8fbb9e25bdadc3590472e629
samuel florez <samuel.f.v.t.p@gmail.com>
2026-02-10 18:56:14 -0500

    almost there

--- src/Components/CardCharacters.tsx
+++ src/Components/CardCharacters.tsx
@@ -1,12 +1,17 @@
 import type { TypesCharacters } from "../types/TypesCharacters"
 
 function Card ({CharacterIndividual}: {CharacterIndividual: TypesCharacters}) {
-
+    
     return (
-        <div className="bg-green-800 w-80 h-100 rounded-2xl flex justify-center">
+        <div className="bg-black/40 w-90 h-105 rounded-2xl flex justify-center flex-col items-center
+        hover:scale-105 transition-all cursor-pointer">
             <img 
-            className="w-70 h-70 rounded-2xl mt-5" 
+            className={`w-70 h-70 rounded-2xl mt-5 opacity-85`} 
             src={CharacterIndividual.image}></img>
+            <p 
+            className="text-2xl mt-2 font-black">{CharacterIndividual.name}</p>
+            <p className="font-bold">{CharacterIndividual.species}</p>
+            <p className={`font-medium ${CharacterIndividual.status == "Alive" ? "text-green-800" : "text-red-800"}`}>{CharacterIndividual.status}</p>
         </div>
     )
 }
--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -4,15 +4,38 @@ import type { TypesCharacters } from "../types/TypesCharacters";
 import Card from "../Components/CardCharacters";
 
 function MainPage() {
-  const [Characters, setCharacters] = useState([]);
+  const [Characters, setCharacters] = useState<TypesCharacters[]>([]);
+  const [Busqueda, setBusqueda] = useState("");
+  const [Loading, setLoading] = useState(true)
+
+  const palabras = Busqueda.toLowerCase().trim().split(" ");
+
+  const resultadosBusqueda = Characters.filter((character) =>
+    palabras.every(
+      (palabra) =>
+        character.name.toLowerCase().includes(palabra) ||
+        character.species.toLowerCase().includes(palabra) ||
+        character.status.toLowerCase().includes(palabra),
+    ),
+  );
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await getStuff();
-      setCharacters(data);
+      setTimeout(() => {
+        setCharacters(data);
+        setLoading(false)
+      }, 2000);
     };
     fetchData();
-  });
+  }, []);
+
+  if(Loading){
+    return (
+    <div className="min-h-screen flex items-center justify-center">
+      <span className="loading loading-infinity loading-xl size-20 bg-green-800"></span>
+    </div>
+  )}
 
   return (
     <div>
@@ -25,29 +48,45 @@ function MainPage() {
         </p>
         <div>
           <input
+            value={Busqueda}
+            type="text"
+            placeholder="Buscar.."
+            onChange={(e) => setBusqueda(e.target.value)}
             className="bg-green-800 p-2 pl-4 pr-4 rounded-4xl mt-5 
             focus:outline-green-600 focus:outline-2 transition-all hover:scale-103 
             focus:scale-105 cursor-pointer"
           ></input>
           <button
+            onClick={() => setBusqueda("")}
             className="bg-green-800 p-2 pl-4 pr-4 rounded-4xl mt-5 transition-all 
         hover:scale-105 cursor-pointer ml-3"
           >
-            X
+            ✘
           </button>
         </div>
+        {/* <span className="loading loading-infinity loading-xl size-20"></span> */}
       </div>
       <div className="flex">
-      <div className="grid grid-cols-1 gap-10 max-w-2xl mx-auto mt-10 ">
-        {Characters.map((CharacterIndividual: TypesCharacters) => {
-          return (
-            <Card
-              key={CharacterIndividual.id}
-              CharacterIndividual={CharacterIndividual}
-            ></Card>
-          );
-        })}
-      </div>
+        <div className="grid grid-cols-1 gap-10 max-w-2xl mx-auto mt-10  bg-black/20 p-10 pl-40 pr-40 rounded-2xl">
+          {
+          resultadosBusqueda.length > 0 ? (
+            resultadosBusqueda.map((CharacterIndividual: TypesCharacters) => {
+              return (
+                <Card
+                  key={CharacterIndividual.id}
+                  CharacterIndividual={CharacterIndividual}
+                ></Card>
+              );
+            })
+          ) : (
+            <div className="flex flex-col justify-center items-center">
+              <p className="text-2xl font-bold text-red-800">No se encontraron</p>
+              <p className="text-2xl font-bold text-red-800">Resultados</p>
+            </div>
+          )
+          }
+        </div>
+        
       </div>
     </div>
   );
