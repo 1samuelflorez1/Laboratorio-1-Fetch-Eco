b571980a503a1b9f7eff531ac9e116528dd99d9a
samuel florez <samuel.f.v.t.p@gmail.com>
2026-02-11 11:05:37 -0500

    spaces

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -6,9 +6,9 @@ import Card from "../Components/CardCharacters";
 function MainPage() {
   const [Characters, setCharacters] = useState<TypesCharacters[]>([]);
   const [Busqueda, setBusqueda] = useState("");
-  const [Loading, setLoading] = useState(true)
-  const [Error, setError] = useState<string>("")
-  const [Online, setOnline] = useState(navigator.onLine)
+  const [Loading, setLoading] = useState(true);
+  const [Error, setError] = useState<string>("");
+  const [Online, setOnline] = useState(navigator.onLine);
 
   const palabras = Busqueda.toLowerCase().trim().split(" ");
 
@@ -20,63 +20,61 @@ function MainPage() {
         character.status.toLowerCase().includes(palabra),
     ),
   );
-
   useEffect(() => {
-    const handleOnline = () => setOnline(true)
-    const handleOffline = () => setOnline(false)
+    const handleOnline = () => setOnline(true);
+    const handleOffline = () => setOnline(false);
 
-    window.addEventListener("online", handleOnline)
-    window.addEventListener("offline", handleOffline)
-  })
-
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+  });
   useEffect(() => {
     const fetchData = async () => {
       try {
         const data = await getStuff();
-  
         setTimeout(() => {
           setCharacters(data);
-          setLoading(false)
+          setLoading(false);
         }, 2000);
-        
-      } catch (error){
-      setError(`${error}`)
-      setLoading(false)
-    }
-  };
-
+      } catch (error) {
+        setError(`${error}`);
+        setLoading(false);
+      }
+    };
     fetchData();
   }, []);
-
-  if(Loading){
+  if (Loading) {
     return (
-    <div className="min-h-screen flex items-center justify-center">
-      <span className="loading loading-infinity loading-xl size-20 bg-green-800"></span>
-    </div>
-  )}
-
-  if(Error){
+      <div className="min-h-screen flex items-center justify-center">
+        <span className="loading loading-infinity loading-xl size-20 bg-green-800"></span>
+      </div>
+    );
+  }
+  if (Error) {
     return (
       <div className="flex flex-col justify-center items-center mt-75">
         <p className="font-bold text-white/40">{Error}</p>
         <button
-        onClick={() => window.location.reload()} 
-        className="bg-white/30 px-4 py-1 rounded-3xl w-30 mt-5 cursor-pointer transition-all hover:bg-red-800">Reintentar</button>
+          onClick={() => window.location.reload()}
+          className="bg-white/30 px-4 py-1 rounded-3xl w-30 mt-5 cursor-pointer transition-all hover:bg-red-800"
+        >
+          Reintentar
+        </button>
       </div>
-    )
+    );
   }
-
-  if(!Online){
+  if (!Online) {
     return (
       <div className="flex flex-col justify-center items-center mt-75">
         <p className="font-bold text-white/40">WiFi Error</p>
         <button
-        onClick={() => window.location.reload()} 
-        className="bg-white/30 px-4 py-1 rounded-3xl w-30 mt-5 cursor-pointer transition-all hover:bg-red-800">Reintentar</button>
+          onClick={() => window.location.reload()}
+          className="bg-white/30 px-4 py-1 rounded-3xl w-30 mt-5 cursor-pointer transition-all hover:bg-red-800"
+        >
+          Reintentar
+        </button>
       </div>
-    )
+    );
   }
-
   return (
     <div>
       <div className="flex flex-col items-center">
@@ -108,8 +106,7 @@ function MainPage() {
       </div>
       <div className="flex">
         <div className="grid grid-cols-1 gap-10 max-w-2xl mx-auto mt-10  bg-black/20 p-10 pl-40 pr-40 rounded-2xl">
-          {
-          resultadosBusqueda.length > 0 ? (
+          {resultadosBusqueda.length > 0 ? (
             resultadosBusqueda.map((CharacterIndividual: TypesCharacters) => {
               return (
                 <Card
@@ -123,13 +120,10 @@ function MainPage() {
               <p className="text-2xl font-bold text-white/40">Data not</p>
               <p className="text-2xl font-bold text-white/40">Found</p>
             </div>
-          )
-          }
+          )}
         </div>
-        
       </div>
     </div>
   );
 }
-
 export default MainPage;
